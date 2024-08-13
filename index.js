40e8f577f6c34b778b9a2b48dab59d87346977c4
popelkin-roman <mymaythe23@gmail.com>
2024-08-13 19:51:02 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const bodyParser = require('body-parser')
+
+
+const app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
